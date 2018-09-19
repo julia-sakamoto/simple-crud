5d3a35eb9a3a5e97c99f16cf5fd9f5727784a676
坂本 樹理亜 <>
2018-09-19 18:39:47 +0900

    client push to db

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import { changeName, changeAge } from '../actions'
+import axios from 'axios'
+import { changeName, changeAge, initializeForm } from '../actions'
 
 const AddForm = ({ store }) => {
   const { name, age } = store.getState().form
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    axios.post('/api/characters', {
+      name,
+      age
+    }).then(res => {
+      console.log(res)
+      store.dispatch(initializeForm())
+    }).catch(err => {
+      console.error(new Error(err))
+    })
+  }
 
   return (
     <div>
-      <form>
+      <form onSubmit={e => handleSubmit(e)}>
         <label>
           名前:
           <input value={name} onChange={e => store.dispatch(changeName(e.target.value))} />
