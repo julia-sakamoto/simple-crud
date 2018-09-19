5a8938e26450bad495911a1aba67ffa056dd94bd
坂本 樹理亜 <>
2018-09-19 18:06:09 +0900

    edited actions, reducers, and App

---
+++ client/src/reducers.js
@@ -0,0 +1,39 @@
+import { combineReducers } from 'redux'
+import { CHANGE_NAME, CHANGE_AGE, INITIALIZE_FORM } from './actions'
+
+const initialState = {
+  form: {
+    name: '',
+    age: ''
+  },
+  characters: {
+    isFetching: false,
+    characterArray: []
+  }
+}
+
+const formReducer = (state = initialState.form, action) => {
+  switch (action.type) {
+    case CHANGE_NAME:
+      return {
+        ...state,
+        name: action.name
+      }
+    case CHANGE_AGE:
+      return {
+        ...state,
+        name: action.age
+      }
+    case INITIALIZE_FORM:
+      return initialState.formReducer
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  form: formReducer,
+  characters: characterReducer
+})
+
+export default rootReducer
